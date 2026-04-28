e3b01a42e013753be53b54b91c44d9b3c45b0cf0
hacknificent <phpdevartem@gmail.com>
2026-04-28 15:11:41 +0200

    Page title added for layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,10 +1,13 @@
+@props([
+    'title' => ''
+])
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ $title ? 'Artem Yerizhenko - ' . $title : 'Artem Yerizhenko' }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
