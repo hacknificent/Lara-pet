700255200f2a627efa6d10d5b1c09034d3ae41c4
hacknificent <phpdevartem@gmail.com>
2026-05-03 15:09:52 +0200

    create_project_ideas migration created

---
+++ database/migrations/2026_05_03_123925_create_ptoject_ideas_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ptoject_ideas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('description');
+            $table->integer('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ptoject_ideas');
+    }
+};
