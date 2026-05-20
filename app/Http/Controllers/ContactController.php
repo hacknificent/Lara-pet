5fe14a3f4f866088e8426bc1e747c6450e708dc8
Artem Yerizhenko <phpdevartem@gmail.com>
2026-05-20 18:16:05 +0200

    error component added to contact form

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -8,6 +8,12 @@ class ContactController extends Controller
 {
     public function contactFormHandler(Request $request)
     {
+        $request->validate([
+            'email' => 'required|email',
+            'full-name' => 'required|string|min:3|max:255',
+            'message' => 'required|string|min:10|max:1000',
+        ]);
+
         return redirect('/contact')->with('success', true);
     }
 }
