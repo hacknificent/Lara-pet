7302e06d67762e1118d882410c623bd3c8cc9139
Artem Yerizhenko <phpdevartem@gmail.com>
2026-06-24 18:16:00 +0200

    Code cleaning

--- app/Http/Controllers/ProjectIdeasController.php
+++ app/Http/Controllers/ProjectIdeasController.php
@@ -8,7 +8,6 @@ use App\Models\ProjectIdea;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Routing\Redirector;
 
 class ProjectIdeasController extends Controller
