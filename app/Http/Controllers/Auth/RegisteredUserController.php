a330410d6f7fef75c20ca1d9619b2a7714c3a724
Artem Yerizhenko <phpdevartem@gmail.com>
2026-07-27 16:33:27 +0200

    Registration is only for guest

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -10,6 +10,12 @@ use Illuminate\Support\Facades\Auth;
 
 class RegisteredUserController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+    
     /**
      * Display a listing of the resource.
      */
