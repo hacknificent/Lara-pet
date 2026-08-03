eced5991d86e2b78d08f6ad40c286bd473372d50
Artem Yerizhenko <phpdevartem@gmail.com>
2026-08-03 17:54:52 +0200

    blades cleaning

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,7 +3,4 @@
     <a href="/" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
         Go to Homepage
     </a>
-    <x-card class="additional-class-test">
-        Card Example
-    </x-card>
 </x-layout>
